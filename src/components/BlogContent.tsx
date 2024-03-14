dbead0bbbdb2a58c6d649a71c09a1decfd651ad2
PerpetuumG <perpetuumg@gmail.com>
2024-03-14 21:27:21 +0200

    Sanity Client to fetch data and Fetching data using groq query

---
+++ src/components/BlogContent.tsx
@@ -0,0 +1,11 @@
+import { Post } from '../../types';
+
+interface Props {
+  posts: Post[];
+}
+
+const BlogContent = ({ posts }: Props) => {
+  return <div>BlogContent</div>;
+};
+
+export default BlogContent;
